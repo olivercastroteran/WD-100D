7019322f78298e26e6957c70a73c4e53a4497aa1
Oliver Castro Teran <olivercastroteran@gmail.com>
2024-02-21 18:29:07 -0400

    Day 30 - Finished

---
+++ 11-IavaScript-basics/day-30_32/app.js
@@ -0,0 +1,12 @@
+let age = 32;
+let username = 'Oliver';
+let hobbies = ['Sports', 'Reading', 'Coding'];
+
+let job = {
+  title: 'Fullstack Developer',
+  salary: 50000,
+  place: 'New York',
+};
+
+console.log(hobbies[1]);
+console.log(job.salary);
