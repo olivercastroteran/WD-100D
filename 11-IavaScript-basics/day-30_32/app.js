e042eda851ffec0589e2ecaeea3862f2cab58d1e
Oliver Castro Teran <olivercastroteran@gmail.com>
2024-02-22 19:04:28 -0400

    Day 31 - Finished

--- 11-IavaScript-basics/day-30_32/app.js
+++ 11-IavaScript-basics/day-30_32/app.js
@@ -8,5 +8,13 @@ let job = {
   place: 'New York',
 };
 
+let adultYears;
+
+function calcAdultYear(userAge) {
+  return userAge - 18;
+}
+
+adultYears = calcAdultYear(42);
+
 console.log(hobbies[1]);
 console.log(job.salary);
