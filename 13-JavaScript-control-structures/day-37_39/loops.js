cf0fdd871c40a8795caa6d2c3cf7618aa2a47bbb
Oliver Castro Teran <olivercastroteran@gmail.com>
2024-03-14 18:00:59 -0400

    Day 39 - Finished

--- 13-JavaScript-control-structures/day-37_39/loops.js
+++ 13-JavaScript-control-structures/day-37_39/loops.js
@@ -1,28 +1,99 @@
-// for (let i = 0; i < 10; i++) {
-//   console.log(i);
-// }
+// First Example: Sum Numbers
 
-// const users = ['Oli', 'Max', 'Jonas'];
+const calcSumBtn = document.querySelector('#calculator button');
+const userNumberInput = document.querySelector('#user-number');
+const resultSum = document.querySelector('#calculated-sum');
 
-// for (const user of users) {
-//   console.log(user);
-// }
+function calcSum() {
+  const number = userNumberInput.value;
+  let sum = 0;
 
-// const loggedInUser = {
-//   name: 'Oli',
-//   age: 31,
-//   isAdmin: true,
-// };
+  for (let i = 0; i <= number; i++) {
+    sum += i;
+  }
 
-// for (const key in loggedInUser) {
-//   console.log(key); // name, age, isAdmin
-//   console.log(loggedInUser[key]); // Oli, 31 true
-// }
+  resultSum.textContent = sum;
+  resultSum.style.display = 'block';
+}
 
-// let isFinished = false;
+calcSumBtn.addEventListener('click', calcSum);
 
-// while (!isFinished) {
-//   isFinished = confirm('Do you want to quit?');
-// }
+// Highlight Links
 
-// console.log('Done!');
+const highlightBtn = document.querySelector('#highlight-links button');
+const links = document.querySelectorAll('#highlight-links a');
+
+function highlightLinks() {
+  for (const link of links) {
+    link.classList.add('highlight');
+  }
+}
+
+highlightBtn.addEventListener('click', highlightLinks);
+
+// Loop Objects
+
+const userData = {
+  name: 'Oli',
+  lastName: 'CT',
+  age: 31,
+};
+
+const showUserDataBtn = document.querySelector('#user-data button');
+const outputUserData = document.querySelector('#output-user-data');
+
+function showUserData() {
+  outputUserData.innerHTML = '';
+
+  for (const key in userData) {
+    const userDataItemElm = document.createElement('li');
+    const txt = key.toUpperCase() + ': ' + userData[key];
+    userDataItemElm.textContent = txt;
+
+    outputUserData.append(userDataItemElm);
+  }
+}
+
+showUserDataBtn.addEventListener('click', showUserData);
+
+// Statistics / Roll the dice
+
+const rollDiceBtn = document.querySelector('#statistics button');
+
+function rollDice() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+function numberOfDiceRolls() {
+  const targetNumber = document.getElementById('user-target-number');
+  const diceRollsList = document.getElementById('dice-rolls');
+
+  const enteredNumber = +targetNumber.value;
+  diceRollsList.innerHTML = '';
+
+  let hasRolledTargetNum = false;
+  let numOfRolls = 0;
+
+  while (!hasRolledTargetNum) {
+    const rolledNum = rollDice();
+    // if (rolledNum === enteredNumber) {
+    //   hasRolledTargetNum = true;
+    // }
+    numOfRolls++;
+
+    const newRollLI = document.createElement('li');
+    newRollLI.textContent = `Roll ${numOfRolls}: ${rolledNum}`;
+
+    diceRollsList.append(newRollLI);
+
+    hasRolledTargetNum = rolledNum === enteredNumber;
+  }
+
+  const totalRollsEl = document.getElementById('output-total-rolls');
+  const outputTargetNum = document.getElementById('output-target-number');
+
+  totalRollsEl.textContent = numOfRolls;
+  outputTargetNum.textContent = enteredNumber;
+}
+
+rollDiceBtn.addEventListener('click', numberOfDiceRolls);
