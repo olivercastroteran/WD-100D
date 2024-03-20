2bf42d6e049609dcb6849fb785323ddb28de5143
Oliver Castro Teran <olivercastroteran@gmail.com>
2024-03-20 13:31:48 -0400

    Day 42 - Finished

--- 14-tic-tac-toe/day-40_44/scripts/app.js
+++ 14-tic-tac-toe/day-40_44/scripts/app.js
@@ -1,5 +1,20 @@
+let playerId = 0;
+
+const players = [
+  {
+    name: '',
+    symbol: 'X',
+  },
+  {
+    name: '',
+    symbol: 'O',
+  },
+];
+
 const playerConfigOverlay = document.getElementById('config-overlay');
 const backdrop = document.getElementById('backdrop');
+const form = document.querySelector('form');
+const errorMsg = document.querySelector('.config-errors');
 
 const editPlayer1Btn = document.querySelector('.edit-player-1-btn');
 const editPlayer2Btn = document.querySelector('.edit-player-2-btn');
@@ -10,3 +25,5 @@ editPlayer2Btn.addEventListener('click', openPlayerConfig);
 
 closeConfigBtn.addEventListener('click', closePlayerConfig);
 backdrop.addEventListener('click', closePlayerConfig);
+
+form.addEventListener('submit', savePlayerConfig);
--- 14-tic-tac-toe/day-40_44/scripts/config.js
+++ 14-tic-tac-toe/day-40_44/scripts/config.js
@@ -1,4 +1,5 @@
-function openPlayerConfig() {
+function openPlayerConfig(e) {
+  playerId = +e.target.dataset.playerid;
   playerConfigOverlay.style.display = 'block';
   backdrop.style.display = 'block';
 }
@@ -6,4 +7,26 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   playerConfigOverlay.style.display = 'none';
   backdrop.style.display = 'none';
+  form.firstElementChild.classList.remove('error');
+  errorMsg.textContent = '';
+  form.firstElementChild.lastElementChild.value = '';
+}
+
+function savePlayerConfig(e) {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const playerName = formData.get('playername').trim();
+
+  if (!playerName) {
+    e.target.firstElementChild.classList.add('error');
+    errorMsg.textContent = 'Please enter a valid name!';
+    return;
+  }
+
+  const updatedPlayerData = document.getElementById(`player-${playerId}-data`);
+  updatedPlayerData.children[1].textContent = playerName;
+
+  players[playerId - 1].name = playerName;
+
+  closePlayerConfig();
 }
