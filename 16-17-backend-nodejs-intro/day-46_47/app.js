49f616df6f9320b4cf14ace6afbdd036cec8a561
Oliver Castro Teran <olivercastroteran@gmail.com>
2024-04-10 17:59:15 -0400

    Day 47 - Finished

---
+++ 16-17-backend-nodejs-intro/day-46_47/app.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+function handleRequest(req, res) {
+  if (req.url === '/currenttime') {
+    res.statusCode = 200;
+    res.end(`<h1>${new Date().toISOString()}</h1>`);
+  } else if (req.url === '/') {
+    res.statusCode = 200;
+    res.end('<h1>Hello World!</h1>');
+  }
+}
+
+const server = http.createServer(handleRequest);
+
+server.listen(3000);
