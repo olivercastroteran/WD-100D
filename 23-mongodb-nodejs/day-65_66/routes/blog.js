95ac655d622469e54e4759d3b3b11a3ad2337db3
Oliver Castro Teran <olivercastroteran@gmail.com>
2024-05-21 16:03:36 -0400

    Day 65 - Finished

---
+++ 23-mongodb-nodejs/day-65_66/routes/blog.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const db = require('../data/database');
+
+const ObjectId = mongodb.ObjectId;
+
+const router = express.Router();
+
+router.get('/', function (req, res) {
+  res.redirect('/posts');
+});
+
+router.get('/posts', function (req, res) {
+  res.render('posts-list');
+});
+
+router.post('/posts', async function (req, res) {
+  const authorId = new ObjectId(req.body.author);
+
+  const author = await db
+    .getDb()
+    .collection('authors')
+    .findOne({ _id: authorId });
+
+  const newPost = {
+    title: req.body.title,
+    summary: req.body.summary,
+    body: req.body.content,
+    date: new Date(),
+    author: {
+      id: authorId,
+      name: author.name,
+      email: author.email,
+    },
+  };
+
+  const result = await db.getDb().collection('posts').insertOne(newPost);
+  console.log(result);
+
+  res.redirect('/posts');
+});
+
+router.get('/new-post', async function (req, res) {
+  const authors = await db.getDb().collection('authors').find().toArray();
+  // console.log(authors);
+  res.render('create-post', { authors });
+});
+
+module.exports = router;
