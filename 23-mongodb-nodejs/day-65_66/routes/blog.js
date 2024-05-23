debb750cb5760efcbc1b124f8decddd1f0b3d92d
Oliver Castro Teran <olivercastroteran@gmail.com>
2024-05-22 23:41:20 -0400

    Day 66 - Finished

--- 23-mongodb-nodejs/day-65_66/routes/blog.js
+++ 23-mongodb-nodejs/day-65_66/routes/blog.js
@@ -11,8 +11,81 @@ router.get('/', function (req, res) {
   res.redirect('/posts');
 });
 
-router.get('/posts', function (req, res) {
-  res.render('posts-list');
+router.get('/posts', async function (req, res) {
+  const posts = await db
+    .getDb()
+    .collection('posts')
+    .find({})
+    .project({ title: 1, summary: 1, 'author.name': 1 })
+    .toArray();
+
+  // console.log(posts);
+
+  res.render('posts-list', { posts });
+});
+
+router.get('/posts/:id', async function (req, res, next) {
+  let postId = req.params.id;
+
+  try {
+    postId = new ObjectId(postId);
+  } catch (error) {
+    return next(error);
+  }
+
+  const post = await db
+    .getDb()
+    .collection('posts')
+    .findOne({ _id: postId }, { summary: 0 });
+
+  if (!post) {
+    return res.status(404).render('404');
+  }
+
+  post.formatedDate = post.date.toLocaleDateString('en-US', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+  post.date = post.date.toISOString();
+
+  res.render('post-detail', { post });
+});
+
+router.get('/posts/:id/edit', async function (req, res) {
+  const postId = req.params.id;
+  const post = await db
+    .getDb()
+    .collection('posts')
+    .findOne({ _id: new ObjectId(postId) }, { title: 1, summary: 1, body: 1 });
+
+  if (!post) {
+    return res.status(404).render('404');
+  }
+
+  res.render('update-post', { post });
+});
+
+router.post('/posts/:id/edit', async function (req, res) {
+  const postId = new ObjectId(req.params.id);
+  const result = await db
+    .getDb()
+    .collection('posts')
+    .updateOne(
+      { _id: postId },
+      {
+        $set: {
+          title: req.body.title,
+          summary: req.body.summary,
+          body: req.body.content,
+        },
+      }
+    );
+
+  console.log(result);
+
+  res.redirect('/posts');
 });
 
 router.post('/posts', async function (req, res) {
@@ -45,6 +118,16 @@ router.get('/new-post', async function (req, res) {
   const authors = await db.getDb().collection('authors').find().toArray();
   // console.log(authors);
   res.render('create-post', { authors });
+});
+
+router.post('/posts/:id/delete', async function (req, res) {
+  const postId = new ObjectId(req.params.id);
+  const result = await db
+    .getDb()
+    .collection('posts')
+    .deleteOne({ _id: postId });
+
+  res.redirect('/posts');
 });
 
 module.exports = router;
