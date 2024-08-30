1275ffe66ffb113e5db5b0675ec15dd5e22f33b2
Oliver Castro Teran <olivercastroteran@gmail.com>
2024-08-30 17:52:25 -0400

    Day 31 - Finished

---
+++ 11-JavaScript-Basics/day-30_33/app.js
@@ -0,0 +1,22 @@
+let age = 32;
+let userName = 'Oli';
+let hobbies = ['Sports', 'Cooking', 'Reading'];
+let job = {
+  title: 'Fullstack Developer',
+  place: 'London',
+  salary: 24000,
+};
+
+// console.log(hobbies[0]);
+// console.log(job.salary);
+
+// let adultYears = age - 18;
+// console.log(adultYears);
+
+// parameter
+function calcAdultYears(userAge) {
+  return userAge - 18;
+}
+
+// argument
+console.log(calcAdultYears(age));
