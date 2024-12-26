2218f8e5ef3033df687f8119a63c47e8d973f8f9
Oliver Castro Teran <olivercastroteran@gmail.com>
2024-12-26 17:12:28 -0400

    Day 38 - Finished

--- 13-JavaScript-Control-Structures/day-37_39/app.js
+++ 13-JavaScript-Control-Structures/day-37_39/app.js
@@ -5,6 +5,19 @@ function updateCount(e) {
   const currentLength = e.target.value.length;
   let remaining = 60 - currentLength;
   countEl.textContent = remaining;
+
+  if (remaining === 0) {
+    inputEl.classList.add('error');
+    countEl.classList.add('error');
+  } else if (remaining <= 10) {
+    inputEl.classList.add('warning');
+    countEl.classList.add('warning');
+    countEl.classList.remove('error');
+    inputEl.classList.remove('error');
+  } else {
+    inputEl.classList.remove('error', 'warning');
+    countEl.classList.remove('error', 'warning');
+  }
 }
 
 inputEl.addEventListener('input', updateCount);
