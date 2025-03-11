bc2fabb9af8205fc74fb98ae63a73b7a7225babd
Oliver Castro Teran <olivercastroteran@gmail.com>
2025-03-10 23:54:58 -0400

    Day 61 - Finished

--- 24-MySQL-NodeJS-Express/day-60_62/routes/blog.js
+++ 24-MySQL-NodeJS-Express/day-60_62/routes/blog.js
@@ -1,17 +1,41 @@
 const express = require('express');
 
+const db = require('../data/database');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.redirect('/posts');
 });
 
-router.get('/posts', (req, res) => {
-  res.render('posts-list');
+router.get('/posts', async (req, res) => {
+  const query = `
+    SELECT posts.*, authors.name AS author_name FROM posts 
+    INNER JOIN authors ON posts.author_id = authors.id
+  `;
+  const [posts] = await db.query(query);
+
+  res.render('posts-list', { posts });
 });
 
-router.get('/new-post', (req, res) => {
-  res.render('create-post');
+router.get('/new-post', async (req, res) => {
+  const [authors] = await db.query('SELECT * FROM authors');
+  res.render('create-post', { authors });
+});
+
+router.post('/posts', async (req, res) => {
+  const data = [
+    req.body.title,
+    req.body.summary,
+    req.body.content,
+    req.body.author,
+  ];
+  await db.query(
+    `INSERT INTO posts (title, summary, body, author_id) VALUES(?)`,
+    [data],
+  );
+
+  res.redirect('/posts');
 });
 
 module.exports = router;
