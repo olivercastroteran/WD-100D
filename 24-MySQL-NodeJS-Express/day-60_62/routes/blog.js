449f18c7574478f286772c24098ea3c103646cdc
Oliver Castro Teran <olivercastroteran@gmail.com>
2025-03-11 00:50:48 -0400

    Day 62 - Finished

--- 24-MySQL-NodeJS-Express/day-60_62/routes/blog.js
+++ 24-MySQL-NodeJS-Express/day-60_62/routes/blog.js
@@ -38,4 +38,66 @@ router.post('/posts', async (req, res) => {
   res.redirect('/posts');
 });
 
+router.get('/posts/:id', async (req, res) => {
+  const query = `
+    SELECT posts.*, authors.name AS author_name, authors.email AS author_email FROM posts 
+    INNER JOIN authors ON posts.author_id = authors.id
+    WHERE posts.id = ?
+  `;
+
+  const [posts] = await db.query(query, [req.params.id]);
+
+  if (!posts || posts.length === 0) {
+    return res.status(404).render('404');
+  }
+
+  const postData = {
+    ...posts[0],
+    date: posts[0].date.toISOString(),
+    humanDate: posts[0].date.toLocaleDateString('en-US', {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    }),
+  };
+
+  res.render('post-detail', { post: postData });
+});
+
+router.get('/posts/:id/edit', async (req, res) => {
+  const query = `
+    SELECT * FROM posts WHERE id = ?
+  `;
+
+  const [posts] = await db.query(query, [req.params.id]);
+
+  if (!posts || posts.length === 0) {
+    return res.status(404).render('404');
+  }
+
+  res.render('update-post', { post: posts[0] });
+});
+
+router.post('/posts/:id/edit', async (req, res) => {
+  const query = `
+    UPDATE posts SET title = ?, summary = ?, body = ?
+    WHERE id = ?
+  `;
+
+  await db.query(query, [
+    req.body.title,
+    req.body.summary,
+    req.body.content,
+    req.params.id,
+  ]);
+
+  res.redirect('/posts');
+});
+
+router.post('/posts/:id/delete', async (req, res) => {
+  await db.query(`DELETE FROM posts WHERE id = ?`, [req.params.id]);
+  res.redirect('/posts');
+});
+
 module.exports = router;
