572bb74ee98439137525b6ba664361a79bc26e23
Oliver Castro Teran <olivercastroteran@gmail.com>
2025-04-24 03:31:41 -0400

    Day 71 - Finished

--- 29-User-Authentication-Sessions/day-70_73/routes/demo.js
+++ 29-User-Authentication-Sessions/day-70_73/routes/demo.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 
 const db = require('../data/database');
 
@@ -22,14 +23,62 @@ router.post('/signup', async function (req, res) {
   const confirmEmail = userData['confirm-email'];
   const password = userData.password;
 
-  const user = { email, password };
+  if (
+    !email ||
+    !confirmEmail ||
+    !password ||
+    password.length.trim() < 6 ||
+    email !== confirmEmail ||
+    !email.includes('@')
+  ) {
+    console.log('Incorrect Data');
+    return res.redirect('/signup');
+  }
+
+  const existingUser = await db
+    .getDb()
+    .collection('users')
+    .findOne({ email: email });
+
+  if (existingUser) {
+    console.log('User exists already');
+    return res.redirect('/signup');
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  const user = { email, password: hashedPassword };
 
   await db.getDb().collection('users').insertOne(user);
 
   res.redirect('/login');
 });
 
-router.post('/login', async function (req, res) {});
+router.post('/login', async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email;
+  const password = userData.password;
+
+  const existingUser = await db
+    .getDb()
+    .collection('users')
+    .findOne({ email: enteredEmail });
+
+  if (!existingUser) {
+    console.log('Could not log in');
+    return res.redirect('/login');
+  }
+
+  const passwordsMatch = await bcrypt.compare(password, existingUser.password);
+
+  if (!passwordsMatch) {
+    console.log('Could not log in - wrong password');
+    return res.redirect('/login');
+  }
+
+  console.log('User is authenticated!');
+  res.redirect('/admin');
+});
 
 router.get('/admin', function (req, res) {
   res.render('admin');
