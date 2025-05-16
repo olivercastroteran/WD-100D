bc6e7e3cc7d8789f939377e60f06a4df44391de8
Oliver Castro Teran <olivercastroteran@gmail.com>
2025-05-15 23:38:43 -0400

    Day 80 - Finished

--- 32-Online-Shop/day-78_92/controllers/auth.controller.js
+++ 32-Online-Shop/day-78_92/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 function getSignup(req, res) {
   res.render('customer/auth/signup');
 }
+
+function signup(req, res) {}
+
 function getLogin(req, res) {
   //...
 }
@@ -8,4 +11,5 @@ function getLogin(req, res) {
 module.exports = {
   getSignup,
   getLogin,
+  signup,
 };
