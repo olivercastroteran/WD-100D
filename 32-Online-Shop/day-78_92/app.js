66cb3ead0f4a17753cd21568aef91ed9075ea3ce
Oliver Castro Teran <olivercastroteran@gmail.com>
2025-05-20 00:47:29 -0400

    Day 81 - Finished

--- 32-Online-Shop/day-78_92/app.js
+++ 32-Online-Shop/day-78_92/app.js
@@ -1,8 +1,13 @@
 const path = require('path');
 
 const express = require('express');
+const csurf = require('csurf');
+const expressSession = require('express-session');
 
+const createSessionConfig = require('./config/session');
 const db = require('./data/database');
+const addCsrfToken = require('./middlewares/csrf-token');
+const errorHandler = require('./middlewares/error-handler');
 const authRoutes = require('./routes/auth.routes');
 
 const app = express();
@@ -11,8 +16,17 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+const sessionConfig = createSessionConfig();
+app.use(expressSession(sessionConfig));
+
+app.use(csurf());
+app.use(addCsrfToken);
 
 app.use(authRoutes);
+
+app.use(errorHandler);
 
 db.connectToDatabase()
   .then(function () {
