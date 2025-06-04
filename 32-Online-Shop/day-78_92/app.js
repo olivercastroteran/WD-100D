ac70419045eaccb0d6c5212c78f7d1d7c1e2f7ae
Oliver Castro Teran <olivercastroteran@gmail.com>
2025-06-04 05:44:25 -0400

    Day 89 - Finished

--- 32-Online-Shop/day-78_92/app.js
+++ 32-Online-Shop/day-78_92/app.js
@@ -4,15 +4,20 @@ const express = require('express');
 const csurf = require('csurf');
 const expressSession = require('express-session');
 
+// Session & DB
 const createSessionConfig = require('./config/session');
 const db = require('./data/database');
+// Middlewares
 const addCsrfToken = require('./middlewares/csrf-token');
 const errorHandler = require('./middlewares/error-handler');
 const checkAuthStatus = require('./middlewares/check-auth');
+const cartMiddleware = require('./middlewares/cart');
+// Routes
 const authRoutes = require('./routes/auth.routes');
 const productsRoutes = require('./routes/products.routes');
 const baseRoutes = require('./routes/base.routes');
 const adminRoutes = require('./routes/admin.routes');
+const cartRoutes = require('./routes/cart.routes');
 const protectRoutes = require('./middlewares/protect-routes');
 
 const app = express();
@@ -23,17 +28,20 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use('/products/assets', express.static('product-data'));
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 const sessionConfig = createSessionConfig();
 app.use(expressSession(sessionConfig));
 
 app.use(csurf());
+app.use(cartMiddleware);
 app.use(addCsrfToken);
 app.use(checkAuthStatus);
 
 app.use(baseRoutes);
 app.use(authRoutes);
 app.use(productsRoutes);
+app.use('/cart', cartRoutes);
 app.use(protectRoutes);
 app.use('/admin', adminRoutes);
 
---
+++ 32-Online-Shop/day-78_92/public/scripts/cart-management.js
@@ -0,0 +1,32 @@
+const addToCartBtn = document.querySelector('#product-details button');
+const cartBadge = document.querySelector('.nav-items .badge');
+
+async function addToCart(e) {
+  let response;
+  try {
+    response = await fetch('/cart/items', {
+      method: 'POST',
+      body: JSON.stringify({
+        productId: e.target.dataset.productid,
+        _csrf: e.target.dataset.csrf,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (error) {
+    alert('Something went wrong');
+    return;
+  }
+
+  if (!response.ok) {
+    alert('Something went wrong');
+  }
+
+  const responseData = await response.json();
+  const newTotalQuantity = responseData.newTotalItems;
+
+  cartBadge.textContent = newTotalQuantity;
+}
+
+addToCartBtn.addEventListener('click', addToCart);
